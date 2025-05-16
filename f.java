9bfba28189c1e1399423a50328bdb446e1512aab
AmishaNaik7 <137600541+AmishaNaik7@users.noreply.github.com>
2025-05-16 19:16:38 +0530

    Add files via upload

---
+++ f.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class f {
+	boolean checkPalindromeString(String input) {
+		 boolean result = true;
+		 int length = input.length();
+
+		 for (int i = 0; i < length/2; i++) {
+		  if (input.charAt(i) != input.charAt(length - i - 1)) {
+		   result = false;
+		   break;
+		  }
+		 }
+
+		 return result;
+		}
+}
